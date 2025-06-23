6ab092f956995675bb88c5aa1b5418a3190003a8
DivineM11 <DMmaduabuchukwu@kellettschool.com>
2025-06-23 04:23:37 +0000

    Jun 23, 2025, 4:23 AM

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from time import sleep
 print("Welcome!")
 sleep(0.5)
+termsofconditions = "Here are the terms and conditions"
+
 play = input("Would u like to play rock paper scissors?(yes/no): ")
 
 if play == "yes":
     print("Brillant! Now we can begin!")
+    print()
+elif play == "no":
+    print("Alright then. I'll hope to see you soon...")
+else:
+    print("I don't understand this gibberish. Type 'YES' or 'NO'!")
